747bd8a0df52e6112e6df79473c459a119044d30
Jose A. Campos <jjoseccampos@gmail.com>
2017-04-22 21:01:29 -0400

    5 solved

---
+++ ruby/Even-or-Odd.rb
@@ -0,0 +1,8 @@
+def even_or_odd(number)
+  if number.even?
+    answer = "Even"
+  else
+    answer = "Odd"
+  end
+return answer
+end
---
+++ ruby/I-love-you-petals.rb
@@ -0,0 +1,18 @@
+def how_much_i_love_you(petals)
+number_x = (petals / 6).to_i
+new_petal = petals - (number_x * 6)
+  if new_petal == 1
+   flower = "I love you"
+  elsif new_petal == 2
+    flower = "a little"
+  elsif new_petal == 3
+    flower = "a lot"
+  elsif new_petal == 4
+    flower = "passionately"
+  elsif new_petal == 5
+    flower = "madly"
+  else new_petal == 6
+    flower = "not at all"
+  end
+return flower
+end
