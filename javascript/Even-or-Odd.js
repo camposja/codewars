2e9fa8b411a14c4168d4ec60249c62b9853aa955
Jose A. Campos <jjoseccampos@gmail.com>
2017-04-26 22:23:56 -0400

    2 javascript katas

---
+++ javascript/Even-or-Odd.js
@@ -0,0 +1,8 @@
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+      var result = "Even"
+  } else {
+      var result = "Odd"
+  }
+  return result
+}
---
+++ javascript/opposites-attract.js
@@ -0,0 +1,11 @@
+function lovefunc(flower1, flower2){
+  var love = true
+  var flowers = flower1 + flower2
+
+  if (flowers % 2 == 0) {
+    var result = false
+  } else {
+    var result = true
+  }
+return result
+}
