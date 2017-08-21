4bc22cf6d5ba9bc969d87872e476177534280598
Jose A. Campos <jjoseccampos@gmail.com>
2017-08-21 14:45:48 -0400

    second solution to fake binary

---
+++ ruby/Count-the-monkeys.rb
@@ -0,0 +1,13 @@
+def monkey_count(n)
+  count = 0
+  array = []
+
+  n.times do
+    if count < n
+      count = count + 1
+      array << count
+    end
+  end
+
+  return array
+end
--- ruby/fake-binary.rb
+++ ruby/fake-binary.rb
@@ -1,3 +1,5 @@
+# First attempt
+
 def fake_bin(s)
   array = s.chars
   array.map! do |i|
@@ -8,4 +10,10 @@ def fake_bin(s)
     end
   end
   return array.join
+end
+
+# Alternative solution (2nd)
+
+def fake_bin(string)
+  string.chars.map { |char| char < '5' ? '0' : '1'}.join
 end
