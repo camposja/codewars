02f02bc3cb81974975cc9aa37d0cccf4ebefba9a
Jose A. Campos <jjoseccampos@gmail.com>
2017-10-09 22:34:50 -0400

    added sentence smash in JS

---
+++ javascript/sentence-smash.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/sentence-smash/train/javascript
+
+// Smash Words
+function smash(words) {
+    "use strict";
+    var result = words.join(" ");
+
+    return result
+};
