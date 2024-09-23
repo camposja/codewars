bfb5fd77b6ec0e74de3441d2ecb92cf848d9da11
Jose A. Campos <jjoseccampos@gmail.com>
2024-09-23 19:17:05 -0400

    updated fizzbuzz in ruby

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,15 +1,11 @@
-result = []
-
-(1...16).each do |number|
+(1...16).map { |e|  } do |number|
   if number % 3 == 0 && number % 5 == 0
-    result << "FizzBuzz"
+    "FizzBuzz"
   elsif number % 5 == 0
-    result << "Buzz"
+    "Buzz"
   elsif number % 3 == 0
-    result << "Fizz"
+    "Fizz"
   else
-    result << number
+    number
   end
 end
-
-puts result
